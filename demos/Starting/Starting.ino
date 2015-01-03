55d03525bd8fbbe5546de9400017b0fce0aba674
Jeff Dyer <jeffdyer@acm.org>
2015-01-03 12:47:40 -0700

    Add calibration function

--- demos/Starting/Starting.ino
+++ demos/Starting/Starting.ino
@@ -1,8 +1,20 @@
 /* -*- Mode: c++; c-indent-level: 2; indent-tabs-mode: nil; tab-width: 2 -*- */
 /* vim: set shiftwidth=2 tabstop=2 autoindent cindent expandtab: */
+/*
+  Before you can draw accurately with Scribot1 you need to calibrate the bot.
+  This is done by executing the 'calibrate()' function and adjusting the values
+  for DEFAULT_CALIBRATION, LF_CALIBRATION, RF_CALIBRATION, LB_CALIBRATION
+  and RB_CALIBRATION.
+
+  Begin by adjusting DEFAULT_CALIBRATION until the bot draws circles where the
+  drawing error is long in at least one direction and short in at least one
+  other. Then interate while adjusting the four specific calibration factors.
+
+  It should be possible to draw circles where the error is less than 1mm in all
+  four directions.
+*/
 
 // BEGIN STEPPER DECLS
-
 #include <AccelStepper.h>
 
 // Define motor pins.
@@ -11,90 +23,119 @@
 #define motorPin3  A2     // Yellow - 28BYJ48 pin 3
 #define motorPin4  A3     // Orange - 28BYJ48 pin 4
 
-#define motorPin5  6    // Blue   - 28BYJ48 pin 1
-#define motorPin6  7     // Pink   - 28BYJ48 pin 2
-#define motorPin7  8    // Yellow - 28BYJ48 pin 3
-#define motorPin8  9    // Orange - 28BYJ48 pin 4
+#define motorPin5  6      // Blue   - 28BYJ48 pin 1
+#define motorPin6  7      // Pink   - 28BYJ48 pin 2
+#define motorPin7  8      // Yellow - 28BYJ48 pin 3
+#define motorPin8  9      // Orange - 28BYJ48 pin 4
 
-#define BACKWARD -1
-#define FORWARD 1
+#define BACKWARD 1
+#define FORWARD -1
 #define SPEED 1000
-#define DELAY 1500  // Minimum delay to allow the motors to complete one step
+#define DELAY 1500        // Minimum delay to allow the motors to complete one step
 
 // Construct stepper objects.
 AccelStepper stepper1(AccelStepper::FULL4WIRE, motorPin1, motorPin3, motorPin2, motorPin4);
 AccelStepper stepper2(AccelStepper::FULL4WIRE, motorPin5, motorPin7, motorPin6, motorPin8);
-
 // END STEPPER DECLS
 
 // BEGIN SERVO DECLS
-
 #include <Servo.h> 
 
 // Construct servo object.
 Servo myservo;
 int pos = 60;    // Servo position 
-
 // END SERVO DECLS
 
-// ADJUST THIS FOR YOUR BOT
-const float CALIBRATE = 3.630;
-const float WHEEL_BASE = 131.0; // mm
-const float CIRCLE_DIAM = 131.0;
+// CALIBRATE THESE VALUES FOR YOUR BOT
+const float DEFAULT_CALIBRATION = 3.63;
+const float LF_CALIBRATION = DEFAULT_CALIBRATION + 0.01;
+const float RF_CALIBRATION = DEFAULT_CALIBRATION + 0.01;
+const float LB_CALIBRATION = DEFAULT_CALIBRATION + 0.02;
+const float RB_CALIBRATION = DEFAULT_CALIBRATION - 0.01;
+const float CIRCLE_DIAM = 131.0;   // Diameter of calibration circles in millimeters
 const float CIRCLE_CIRCUM = CIRCLE_DIAM * PI;
-const float CIRCLE_STEPS = 13068;  // From console
-const float STEP_LENGTH = 2 * CIRCLE_CIRCUM / CIRCLE_STEPS;
-
-int leftStepCount = 0;
-int rightStepCount = 0;
+const float CIRCLE_STEPS = 3600 * DEFAULT_CALIBRATION;
+const float STEP_LENGTH = 2 * CIRCLE_CIRCUM / CIRCLE_STEPS; // 2x because wheel travels twice as far
 
 void setup(void)
 {
   Serial.begin(115200);
-
   // Initialize steppers and servo.
   stepper1.setMaxSpeed(SPEED);
   stepper2.setMaxSpeed(SPEED);
   myservo.attach(A4);
   penUp();  // Pen up to avoid unintended marks
-
   // PUT COMMANDS HERE
-  circle();
-  penUp();
-  step(-450, 450);
-  move(-50);
-  square();
-  step(225, -225);
-  move(100);
-  triangle();
-  penUp();  // Pen up to avoid unintended marks
+  //  calibrate();
+  //  square(100);
+  // triangle(100);
+  arc(10, 3600);
 }
 
 void loop(void)
 {
 }
 
-float millimetersToSteps(float length)
+float millimetersToSteps(float distance)
 {
-  return length / STEP_LENGTH / CALIBRATE;
+  float calibration = distance > 0
+    ? (LF_CALIBRATION + RF_CALIBRATION) / 2
+    : (LB_CALIBRATION + RB_CALIBRATION) / 2;  // Get average of left and right
+  return distance / STEP_LENGTH / calibration;
 }
 
-void line(float len)
+void calibrate()
 {
-  int steps = millimetersToSteps(len);
+  penDown();
+  step(3600, 0);  // LF
+  penUp();
+  step(-450, 450);
+  step(100, 100);
+  step(-450, 450);
+  penDown();
+  step(0, 3600);  // RF
+  penUp();
+  step(450, -450);
+  step(100, 100);
+  step(450, -450);
+  penDown();
+  step(-3600, 0);  // LB
+  penUp();
+  step(-450, 450);
+  step(100, 100);
+  step(-450, 450);
+  penDown();
+  step(0, -3600);  // RB
+  penUp();
+}
+
+void line(float distance)
+{
+  int steps = millimetersToSteps(distance);
   penDown();
   step(steps, steps);
   penUp();
 }
 
-void move(float len)
+void move(float distance)
 {
-  int steps = millimetersToSteps(len);
+  int steps = millimetersToSteps(distance);
   penUp();
   step(steps, steps);
 }
 
-void circle()
+void arc(int radius, int angle)
+{
+  int leftSteps, rightSteps;
+  int ratio = 2 * radius / CIRCLE_DIAM;
+  leftSteps = 1800 + CIRCLE_STEPS * ratio;
+  rightSteps = -3600 + leftSteps;
+  penDown();
+  step(leftSteps, rightSteps);
+  penUp();
+}  
+
+void circle(int radius)
 {
   penDown();
   step(-1000, 2600);
@@ -115,30 +156,26 @@ void circleRight()
   penUp();
 }  
 
-void square()
+void square(int size)
 {
-  penDown();
-  line(100);
+  line(size);
   step(450, -450);
-  line(100);
+  line(size);
   step(450, -450);
-  line(100);
+  line(size);
   step(450, -450);
-  line(100);
+  line(size);
   step(450, -450);
-  penUp();
 }  
 
-void triangle()
+void triangle(int size)
 {
-  penDown();
-  line(70);
+  line(size);
   step(600, -600);
-  line(70);
+  line(size);
   step(600, -600);
-  line(70);
+  line(size);
   step(600, -600);
-  penUp();
 }  
 
 void penUp()
@@ -167,14 +204,10 @@ void penDown()
 
 void step(long lsteps, long rsteps)
 {
-  lsteps = CALIBRATE * lsteps;
-  rsteps = CALIBRATE * rsteps;
-  leftStepCount += lsteps;
-  rightStepCount += rsteps;
-  int dirL = (lsteps > 0) ? BACKWARD : FORWARD;
-  int dirR = (rsteps > 0) ? BACKWARD : FORWARD;
-  lsteps = abs(lsteps);
-  rsteps = abs(rsteps);
+  int dirL = (lsteps > 0) ? FORWARD : BACKWARD;
+  int dirR = (rsteps > 0) ? FORWARD : BACKWARD;
+  lsteps = abs(lsteps) * (dirL == FORWARD ? LF_CALIBRATION : LB_CALIBRATION);
+  rsteps = abs(rsteps) * (dirR == FORWARD ? RF_CALIBRATION : RB_CALIBRATION);
   if (lsteps >= rsteps)
   {
     double offset = 0;
